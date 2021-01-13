a4447e2dec8068c23704ba3fb934b48c98f58f5a
VacusTechnology <rgusai97@gmail.com>
2021-01-13 15:47:52 +0530

    Updated views

--- backend/gearup/data/views.py
+++ backend/gearup/data/views.py
@@ -39,7 +39,8 @@ class lists(APIView):
         authentication_classes = [SessionAuthentication]
         permission_classes = [IsAuthenticated]
 
-        board=Board.objects.get(name=boardname)
+        profile=request.user.profile
+        board=Board.objects.get(name=boardname,profile=profile)
         lists=List.objects.filter(board=board)
         listsSerializer = ListSerializer(lists,many=True)
         if listsSerializer.is_vallid():
@@ -52,7 +53,8 @@ class tasks(APIView):
         authentication_classes = [SessionAuthentication]
         permission_classes = [IsAuthenticated]
 
-        board=Board.objects.get(name=boardname)
+        profile=request.user.profile
+        board=Board.objects.get(name=boardname,profile=profile)
         listObject=List.objects.get(name=listname,board=board)
         tasks = Task.objects.filter(list=listObject)
         tasksSerializer = TaskSerializer(tasks,many=True)
@@ -60,4 +62,146 @@ class tasks(APIView):
             return Response(tasksSerializer.data,status=status.HTTP_200_OK)
         else:
             return Response(tasksSerializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
+
+
+#Board related APIs
+class addBoard(APIView):
+    def post(self,request,boardname):
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+        
+        try:
+            profile=request.user.profile
+            board = Board.objects.create(name=boardname,lists=0,cards=0,profile=profile)
+            return Response({"name":boardname},status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class deleteBoard(APIView):
+    def post(self,request,boardname):
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+        
+        try:
+            profile=request.user.profile
+            board = Board.objects.get(name=boardname,profile=profile)
+            board.delete()
+            return Response({"name":boardname},status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class renameBaord(APIView):
+    def post(self,request,boardname):
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+        
+        try:
+            profile=request.user.profile
+            board = Board.objects.get(name=boardname,profile=profile)
+            board.name=request.data.get('newname')
+            board.save()
+            return Response({"name":boardname},status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+#List Related APIs
+class addList(APIView):
+    def post(self,request,boardname,listname):
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+                
+        try:
+            profile=request.user.profile
+            board=Board.objects.get(name=boardname,profile=profile)
+            List.objects.create(name=listname,board=board)
+            return Response({"name":listname},status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class deleteList(APIView):
+    def post(self,request,boardname,listname):
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+                
+        try:
+            profile=request.user.profile
+            board=Board.objects.get(name=boardname,profile=profile)
+            List.objects.get(name=listname,board=board).delete()
+            return Response({"name":listname},status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class renameList(APIView):
+    def post(self,request,boardname,listname):
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+                
+        try:
+            profile=request.user.profile
+            board=Board.objects.get(name=boardname,profile=profile)
+            List=List.objects.get(name=listname,board=board)
+            List.name=listname
+            List.save()
+            return Response({"name":boardname},status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+#Task Related APIs
+class addTask(APIView):
+    def post(self,request,boardname,listname):
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+       
+        try:
+            profile=request.user.profile
+            board=Board.objects.get(name=boardname,profile=profile)
+            List=List.objects.get(name=listname,board=board)
+            
+            taskPayload = request.data.get("data")
+            # Add task addition related code
+            # 
+            # 
+            
+            return Response({"id":Task.objects.all()[Task.objects.count()-1].id},status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class editTask(APIView):
+    def post(self,request,boardname,listname,tid):
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+        
+        try:
+            profile=request.user.profile
+            board=Board.objects.get(name=boardname,profile=profile)
+            List=List.objects.get(name=listname,board=board)
+            task=Task.objects.get(id=tid,List=List)
+
+            #Add Task edit related code here
+            # 
+            #     
+       
+            return Response({"id":tid},status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class deletetask(APIView):
+    def post(self,request,boardname,listname,tid):
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+        
+        try:
+            profile=request.user.profile
+            board=Board.objects.get(name=boardname,profile=profile)
+            List=List.objects.get(name=listname,board=board)
+            task=Task.objects.get(id=tid,List=List)
+            task.delete()
+            return Response({"id":tid},status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
